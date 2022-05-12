0080d1af3fbd1d39ca05041f02a59f3a1d22b58c
Tianhaitao <2804622087@qq.com>
2022-05-12 20:07:50 +0800

    完成了Ajax请求，但没响应

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,23 +1,53 @@
 $(function() {
+    // 登录和注册的切换
     $('#line_res').click(function() {
         $('.table_enroll').hide();
         $('.table_register').show();
     })
     $('#line_enroll').click(function() {
-        $('.table_enroll').show();
-        $('.table_register').hide();
-    })
+            $('.table_enroll').show();
+            $('.table_register').hide();
+        })
+        // 表单验证
     var form = layui.form;
     // console.log(form);
     form.verify({
-        repsw: function(value) {
-            let val = $('#password').val();
-            if (value != val) {
-                return '两次密码输入不一致';
-            }
-        },
-        psw: [
-            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
-        ]
+            repsw: function(value) {
+                let val = $('#password').val();
+                if (value != val) {
+                    return '两次密码输入不一致';
+                }
+            },
+            psw: [
+                /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+            ]
+        })
+        // 发起ajax请求
+        // $('#form1').submit(function(event) {
+        //     // console.log(1);
+        //     alert(1)
+        //     event.preventDefault();
+        //     // $.ajax({
+        //     //     method: 'get',
+        //     //     url: 'https://www.showdoc.cc/escook?page_id=3707158761215217',
+        //     //     success: function(res) {
+        //     //         console.log(res);
+        //     //     }
+        //     // })
+        // })
+    $('#form').on('submit', function(event) {
+        event.preventDefault();
+    })
+    $('#form2').on('submit', function(event) {
+        event.preventDefault();
+        data = {
+            username: $('#username').val(),
+            password: $('#password').val()
+        }
+
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
+            console.log(res);
+        })
+        console.log(1);
     })
 })
