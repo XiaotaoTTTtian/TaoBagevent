860d951a294d03003e0620e3c22f9cfc4a626a1f
Tianhaitao <2804622087@qq.com>
2022-05-14 13:46:49 +0800

    完成了Ajax请求

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,49 +10,46 @@ $(function() {
         })
         // 表单验证
     var form = layui.form;
-    // console.log(form);
+    var layer = layui.layer;
+    // console.log(layer);
     form.verify({
-            repsw: function(value) {
-                let val = $('#password').val();
-                if (value != val) {
-                    return '两次密码输入不一致';
-                }
-            },
-            psw: [
-                /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
-            ]
-        })
-        // 发起ajax请求
-        // $('#form1').submit(function(event) {
-        //     // console.log(1);
-        //     alert(1)
-        //     event.preventDefault();
-        //     // $.ajax({
-        //     //     method: 'get',
-        //     //     url: 'https://www.showdoc.cc/escook?page_id=3707158761215217',
-        //     //     success: function(res) {
-        //     //         console.log(res);
-        //     //     }
-        //     // })
-        // })
+        repsw: function(value) {
+            let val = $('#password').val();
+            if (value != val) {
+                return '两次密码输入不一致';
+            }
+        },
+        psw: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ]
+    })
     $('#form').on('submit', function(event) {
         event.preventDefault();
         const data = $(this).serialize();
         console.log(data);
         $.post('http://www.liulongbin.top:3007/api/login', data, function(res) {
             console.log(res);
+            if (res.status != 0) {
+                return alert('登录失败');
+            }
+            localStorage.setItem('key', res.token);
+            location.href = 'http://127.0.0.1:5500/index.html';
         })
     })
     $('#form2').on('submit', function(event) {
         event.preventDefault();
-        data = {
-            username: $('#username').val(),
-            password: $('#password').val()
-        }
-
+        data = $(this).serialize();
         $.post('http://www.liulongbin.top:3007/api/reguser', data, function(res) {
             console.log(res);
+            if (res.status != 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg('注册成功', {
+                icon: 1,
+                time: 2000 //2秒关闭（如果不配置，默认是3秒）
+            }, function() {
+                $('#line_enroll').click();
+            });
         })
-        console.log(1);
     })
 })
